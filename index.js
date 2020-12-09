ac9c80f04e6f1cdccef8d8064f5ed10b668b36fc
hicksey <hicksey14@gmail.com>
2020-12-08 21:24:07 -0700

    define images, modals, spans, and get modals in index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+
+/* get the modals */
+var modal1 = document.getElementById('imgModal1');
+var modal2 = document.getElementById('imgModal2');
+var modal3 = document.getElementById('imgModal3');
+var modal4 = document.getElementById('imgModal4');
+var modal5 = document.getElementById('imgModal5');
+var modal6 = document.getElementById('imgModal6');
+var modal7 = document.getElementById('imgModal7');
+var modal8 = document.getElementById('imgModal8');
