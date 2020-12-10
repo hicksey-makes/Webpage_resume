4362b4844d271e10554185d7d84c2ee9f632e121
hicksey <hicksey14@gmail.com>
2020-12-10 15:08:04 -0700

    add text shadow to nav a on hover, adjust work radial gradient, prettify js code

--- index.js
+++ index.js
@@ -1,84 +1,79 @@
-/* Get the Modal */
+/* Get the Modal, modal img and caption text */
 var modal1 = document.getElementById('imgModal1');
+var modalImg1 = document.getElementById('img-01');
+var captionText = document.getElementById('caption');
 
-/* get image and insert in modal - use its "alt" text as a caption */
+
+/* get first certificate image and insert in modal - use its "alt" text as a caption */
 var img1 = document.getElementById('img-first');
-var modalImg1 = document.getElementById('img-01');
-var captionText = document.getElementById('caption');
 img1.onclick = function() {
   modal1.style.display = "block";
   modalImg1.src = this.src;
   captionText.innerHTML = this.alt;
 }
 
-/* get image and insert in modal - use its "alt" text as a caption */
+/* second certificate image */
 var img1 = document.getElementById('img-second');
-var modalImg1 = document.getElementById('img-01');
-var captionText = document.getElementById('caption');
-img1.onclick = function() {
-  modal1.style.display = "block";
-  modalImg1.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-/* get image and insert in modal - use its "alt" text as a caption */
-var img1 = document.getElementById('img-third');
-var modalImg1 = document.getElementById('img-01');
-var captionText = document.getElementById('caption');
-img1.onclick = function() {
-  modal1.style.display = "block";
-  modalImg1.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-/* get image and insert in modal - use its "alt" text as a caption */
-var img1 = document.getElementById('img-fourth');
-var modalImg1 = document.getElementById('img-01');
-var captionText = document.getElementById('caption');
-img1.onclick = function() {
-  modal1.style.display = "block";
-  modalImg1.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-/* get image and insert in modal - use its "alt" text as a caption */
-var img1 = document.getElementById('img-fifth');
-var modalImg1 = document.getElementById('img-01');
-var captionText = document.getElementById('caption');
-img1.onclick = function() {
-  modal1.style.display = "block";
-  modalImg1.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-/* get image and insert in modal - use its "alt" text as a caption */
-var img1 = document.getElementById('img-sixth');
-var modalImg1 = document.getElementById('img-01');
-var captionText = document.getElementById('caption');
-img1.onclick = function() {
-  modal1.style.display = "block";
-  modalImg1.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-/* get image and insert in modal - use its "alt" text as a caption */
-var img1 = document.getElementById('img-seventh');
-var modalImg1 = document.getElementById('img-01');
-var captionText = document.getElementById('caption');
-img1.onclick = function() {
-  modal1.style.display = "block";
-  modalImg1.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-/* get image and insert in modal - use its "alt" text as a caption */
-var img1 = document.getElementById('img-eighth');
-var modalImg1 = document.getElementById('img-01');
-var captionText = document.getElementById('caption');
 img1.onclick = function() {
   modal1.style.display = "block";
   modalImg1.src = this.src;
   captionText.innerHTML = this.alt;
 }
 
-// Get the <span> element that closes the modal
+/* third certificate image */
+var img1 = document.getElementById('img-third');
+img1.onclick = function() {
+  modal1.style.display = "block";
+  modalImg1.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+/* fourth certificate image */
+var img1 = document.getElementById('img-fourth');
+img1.onclick = function() {
+  modal1.style.display = "block";
+  modalImg1.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+/* fifth certificate image */
+var img1 = document.getElementById('img-fifth');
+img1.onclick = function() {
+  modal1.style.display = "block";
+  modalImg1.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+/* sixth certificate image */
+var img1 = document.getElementById('img-sixth');
+img1.onclick = function() {
+  modal1.style.display = "block";
+  modalImg1.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+/* seventh certificate image */
+var img1 = document.getElementById('img-seventh');
+img1.onclick = function() {
+  modal1.style.display = "block";
+  modalImg1.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+/* eighth certificate image */
+var img1 = document.getElementById('img-eighth');
+img1.onclick = function() {
+  modal1.style.display = "block";
+  modalImg1.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+
+/* Get the <span> element that closes the modal */
 var span1 = document.getElementsByClassName("exit")[0];
 
-// When the user clicks on <span> (x), close the modal
+
+/* close the modal onclick of the <span> x */
 span1.onclick = function() {
   modal1.style.display = "none";
 }
