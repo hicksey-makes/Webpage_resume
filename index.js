9576a958fcf6c2ac2a61356508bf3d472d116255
hicksey <hicksey14@gmail.com>
2020-12-14 15:52:51 -0700

    add uber prof thumbnail, new modal with same animation as cert.s

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ var modal1 = document.getElementById('imgModal1');
 var modalImg1 = document.getElementById('img-01');
 var captionText = document.getElementById('caption');
 
+var modal2 = document.getElementById('imgModal2');
+var modalImgUb = document.getElementById('img-ub');
+var captionText2 = document.getElementById('caption2');
+
+var img1 = document.getElementById('img-ninth');
+img1.onclick = function() {
+  modal2.style.display = "block";
+  modalImgUb.src = this.src;
+  captionText2.innerHTML = this.alt;
+}
 
 /* get first certificate image and insert in modal - use its "alt" text as a caption */
 var img1 = document.getElementById('img-first');
@@ -71,9 +81,14 @@ img1.onclick = function() {
 
 /* Get the <span> element that closes the modal */
 var span1 = document.getElementsByClassName("exit")[0];
+var span2 = document.getElementsByClassName("exit2")[0];
 
 
 /* close the modal onclick of the <span> x */
 span1.onclick = function() {
   modal1.style.display = "none";
+}
+
+span2.onclick = function() {
+  modal2.style.display = "none";
 }
