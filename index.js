b58aec81ae4ff2c9f066fba939be12a40f3cc5dc
hicksey <hicksey14@gmail.com>
2021-09-13 10:11:36 -0600

    update contentToPrint substring for pdf gen

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ $('.navbar li a').click(function(event) {
 $("#PrintNow").on("click", function () {
             var divContents = $("#html-page").html();
             console.log(divContents.indexOf(`<div id="footStuff">`));
-            var contentToPrint = divContents.substring(2267, 18746);
+            var contentToPrint = divContents.substring(2267, 18745);
             console.log(contentToPrint);
             var printWindow = window.open('', '', 'height=400,width=800');
             printWindow.document.write('<html><head><title>Sam-H-Resume</title>');
